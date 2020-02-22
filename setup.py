f54e5d23c671a9fc84a1548d334cb1033759243c
hirorittsu <zq6295migly@gmail.com>
2020-02-22 15:54:57 +0900

    [fix]ros2動作確認

--- setup.py
+++ setup.py
@@ -7,14 +7,16 @@ setup(
     version='0.0.0',
     packages=[],
     py_modules=[
-        'scripts.human_detection_scan',
+        'script.human_detection_scan',
+        'script.human_detection_predict',
+        'script.human_detection_calculation',
     ],
     data_files=[
         ('share/ament_index/resource_index/packages',
          ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools', 'cv2', 'numpy'],
+    install_requires=['setuptools'],
     zip_safe=True,
     maintainer='hirorittsu',
     maintainer_email='zq6295migly@gmail.com',
@@ -23,7 +25,9 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'human_detection_scan_node = scripts.human_detection_scan:main'
+            'scan = script.human_detection_scan:main',
+            'predict = script.human_detection_predict:main',
+            'calculation = script.human_detection_calculation:main',
         ],
     },
 )
