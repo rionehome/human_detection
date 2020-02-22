12f60af271fb8ebc5c320cf81302c1f9d627ed5a
hirorittsu <zq6295migly@gmail.com>
2020-02-22 14:58:45 +0900

    [add]推論ノードと計算ノード追加

---
+++ scripts/human_detection_calculation.py
@@ -0,0 +1,31 @@
+import cv2
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+from sensor_msgs.msg import Image, PointCloud2
+import numpy as np
+
+
+class HumanDetectionPredict(Node):
+
+    def __init__(self, node_name: str):
+        super().__init__(node_name)
+        self.is_start = False
+        self.color_image = None
+        self.create_subscription(String, "/human_detection/command", self.callback_command, 10)
+
+    def callback_command(self, msg):
+        if msg.data == "predict":
+            self.is_start = True
+        else:
+            self.is_start = False
+
+
+def main():
+    rclpy.init()
+    node = HumanDetectionPredict("HumanDetectionPredict")
+    rclpy.spin(node)
+
+
+if __name__ == '__main__':
+    main()
