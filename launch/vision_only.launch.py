dacf19bac7aa530df909d456fc26904b112ad9b2
hirorittsu <zq6295migly@gmail.com>
2020-02-25 18:08:25 +0900

    [add]launch追加

---
+++ launch/vision_only.launch.py
@@ -0,0 +1,93 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import LogInfo
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        #############################################################
+        LogInfo(
+            msg="launch slam_gmapping"
+        ),
+        Node(
+            package='slam_gmapping',
+            node_executable='slam_gmapping',
+            output='screen'
+        ),
+        #############################################################
+        LogInfo(
+            msg="launch turn_robot"
+        ),
+        Node(
+            package="turn_robot",
+            node_executable="turn_robot",
+        ),
+        #############################################################
+        LogInfo(
+            msg="launch turtlebot_bringup"
+        ),
+        Node(
+            package="ydlidar",
+            node_executable="ydlidar_node",
+            output="screen",
+            parameters=["ydlidar.yaml"]
+        ),
+        Node(
+            package="turtlebot_bringup",
+            node_executable="turtlebot2",
+            output="screen",
+            parameters=["turtlebot2.yaml"]
+        ),
+        #############################################################
+        LogInfo(
+            msg="launch realsense_ros2_camera"
+        ),
+        Node(
+            package="realsense_ros2_camera",
+            node_executable="realsense_ros2_camera",
+            output="screen"
+        ),
+        #############################################################
+        LogInfo(
+            msg="launch face_predictor"
+        ),
+        Node(
+            package="face_predictor",
+            node_executable="face_predictor",
+            output="screen"
+        ),
+        #############################################################
+        LogInfo(
+            msg="launch human_detection"
+        ),
+        Node(
+            package="human_detection",
+            node_executable="scan_main",
+            output="screen"
+        ),
+        Node(
+            package="human_detection",
+            node_executable="scan_image",
+            output="screen"
+        ),
+        Node(
+            package="human_detection",
+            node_executable="scan_odometry",
+            output="screen"
+        ),
+        Node(
+            package="human_detection",
+            node_executable="scan_xyz",
+            output="screen"
+        ),
+        Node(
+            package="human_detection",
+            node_executable="predict",
+            output="screen"
+        ),
+        Node(
+            package="human_detection",
+            node_executable="calculation",
+            output="screen"
+        ),
+    ])
