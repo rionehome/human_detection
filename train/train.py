57a28f6f19c78a181c4bd70b994c1604740067d5
hirorittsu <zq6295migly@gmail.com>
2020-03-13 01:08:03 +0900

    [update]学習完了

--- train/train.py
+++ train/train.py
@@ -7,7 +7,7 @@ from keras import models
 from keras import layers
 from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
-
+import numpy as np
 from lib.tools import save_history, show_image_tile
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -17,15 +17,38 @@ LOG_PATH = os.path.join("log/", datetime.now().strftime('%Y%m%d_%H%M%S'))
 
 IMAGE_SIZE = 96
 BATCH_SIZE = 100
-NUM_EPOCH = 100
+NUM_EPOCH = 10
+
+
+class CustomImageDataGenerator(ImageDataGenerator):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def random_transform(self, x, seed=1):
+        x = super().random_transform(x, seed=seed)
+        # xはnp.float32型になっている
+        x = self.addGaussianNoise(x, seed=seed)
+
+        return x
+
+    def addGaussianNoise(self, src, seed):
+        row, col, ch = src.shape
+        mean = 0
+        var = 0.1
+        sigma = 15
+        gauss = np.random.normal(mean, sigma, (row, col, ch))
+        gauss = gauss.reshape(row, col, ch)
+        noisy = src + gauss
+
+        return noisy
 
 
 def create_model():
     input_data = layers.Input(shape=(IMAGE_SIZE, IMAGE_SIZE, 3))
     vgg16_model = VGG16(include_top=False)
-    vgg16_model.trainable = True
-    # for layer in vgg16_model.layers[:15]:
-    #    layer.trainable = False
+    # vgg16_model.trainable = True
+    for layer in vgg16_model.layers[:15]:
+        layer.trainable = False
     flatten = layers.Flatten()(vgg16_model(input_data))
     dense1 = layers.Dense(256, activation='relu')(flatten)
     dense1 = layers.Dropout(0.3)(dense1)
@@ -83,7 +106,7 @@ def train():
 
     history = model.fit_generator(
         train_data_iterator,
-        steps_per_epoch=1000,
+        steps_per_epoch=50,
         epochs=NUM_EPOCH,
         validation_data=val_data_iterator,
         validation_steps=50,
